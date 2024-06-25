def9108e8a5a321606d8bf9284fc7171f887050d
arham2002 <arham21102002@gmail.com>
2024-06-25 10:49:18 -0400

    added fourth time

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
-def index(request):
- return render(request, 'index.html', {})
+class MenuItemView(generics.ListCreateAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+  queryset = Booking.objects.all()
+  serializer_class = BookingSerializer
